a3b56221e8d311b84ada513d9a4343c1cf8a9814
Artem Voronyi <voronyi.artem@gmail.com>
2023-08-04 12:08:51 +0300

    feat: add quots accross js code

--- Js/index.js
+++ Js/index.js
@@ -3,6 +3,7 @@ const button = document.querySelector('.button');
 
 button.onclick = function () {
   let result = document.querySelector('.input').value;
+  result = "« " + result + " »"
   document.getElementById('result').innerHTML = result;
-  
+
 }
